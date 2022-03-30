23d9cc2c0a96852523692aec9b25258f97f4bb80
WANESSA WAZMUTH <74206277+WanessaWazmuth@users.noreply.github.com>
2022-03-30 17:25:09 -0300

    Add files via upload

---
+++ springsecurity-login/springsecurity-login/src/main/java/com/login/springsecuritylogin/SpringsecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.springsecuritylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityLoginApplication.class, args);
+	}
+
+}
